2b370c021d0edfadf0b997c147f9eadd35ac6fa1
teo <teo.ferizovic@hotmail.com>
2016-04-20 11:22:07 +0200

    Remove unnesesary words

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.post('/checkin', function (req, res, next) {
   var userName = req.body.user_name;
   var date = Date();
   var botPayload = {
-    text : 'Hello ' + userName + ', you came to work atas '+date
+    text : 'Hello ' + userName + ', you came to work on '+date
   };
   
   if (userName !== 'slackbot') {
@@ -34,7 +34,7 @@ app.post('/checkout', function (req, res, next) {
   var userName = req.body.user_name;
   var date = Date();
   var botPayload = {
-    text : 'Bye ' + userName + ', you finished your work ats '+date
+    text : 'Bye ' + userName + ', you finished your work on '+date
   };
   
   if (userName !== 'slackbot') {
