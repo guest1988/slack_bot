0da686a1aa883f6cac5e7b9a59c9dd4a79e84fc8
Teufik Ferizović <tf@snowfall.se>
2016-05-04 16:31:18 +0200

    Added some changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/checkin', function (req, res, next) {
   };
   
   if (userName !== 'slackbot') {
-    return res.status(200).json('#checkin !!');
+    return res.status(200).json(botPayload);
   } else {
     return res.status(200).end();
   }
